a9e248fcc879ebc2d0502c3d3da467df64090048
wafaamuhammad123 <wafaa.muhammad2000@gmail.com>
2023-04-15 00:14:04 +0200

    lab2 part2

--- lab2/src/app/second/second.component.ts
+++ lab2/src/app/second/second.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./second.component.css']
 })
 export class SecondComponent {
+  imgSrc = [
+    'assets/Images/img1.jpg',
+    'assets/Images/img2.jpeg',
+    'assets/Images/img3.jpeg',
+    'assets/Images/img4.jpeg'
+  ];
+  img="assets/Images/1.jpg";
+  loc=0;
+
+  imgInterval:any;
+  
+  prevImage(){
+    if(this.loc>0){
+      this.loc--;
+    this.img = this.imgSrc[this.loc];
+    }
+  }
+
+  nextImage(){
+    if(this.loc<3){
+      this.loc++;
+      this.img = this.imgSrc[this.loc];
+    } 
+  }
+
+  slideImage(){
+    this.imgInterval = setInterval(() => {
+      this.loc++;
+      if(this.loc>3){
+        this.loc=0;
+      }
+      this.img = this.imgSrc[this.loc];
+    }, 1000);
+  }
+
+  stopImage(){
+    clearInterval(this.imgInterval);
+  }
 
 }
